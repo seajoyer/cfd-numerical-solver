7ded1afa186f18becf10ace11c28aae27819189b
VaheVahanyan <vahanyan.vav@gmail.com>
2025-11-17 05:47:32 +0300

    Add new solvers to SolverFactory

--- src/solver/SolverFactory.cpp
+++ src/solver/SolverFactory.cpp
@@ -1,5 +1,7 @@
 #include "solver/SolverFactory.hpp"
+#include "solver/GodunovKolganRodionovSolver.hpp"
 #include "solver/GodunovSolver.hpp"
+#include "solver/AnalyticalSolver.hpp"
 #include <algorithm>
 #include <cctype>
 #include <stdexcept>
@@ -10,21 +12,19 @@ auto SolverFactory::Create(Settings &settings) -> std::unique_ptr<Solver> {
     std::transform(type_lower.begin(), type_lower.end(), type_lower.begin(),
                    [](unsigned char c) { return std::tolower(c); });
 
+    if (type_lower == "analytical") {
+        return std::make_unique<AnalyticalSolver>(settings);
+    }
     if (type_lower == "godunov") {
+        settings.reconstruction = "p0";
         return std::make_unique<GodunovSolver>(settings);
     }
-
-    // Future solvers can be added here:
-    // 
-    // if (type_lower == "muscl") {
-    //     return std::make_unique<MUSCLSolver>(dim);
-    // }
-    // 
-    // if (type_lower == "weno") {
-    //     return std::make_unique<WENOSolver>(dim);
-    // }
-    //
-    // etc ...
+    if (type_lower == "godunov-kolgan") {
+        return std::make_unique<GodunovSolver>(settings);
+    }
+    if (type_lower == "godunov-kolgan-rodionov") {
+        return std::make_unique<GodunovKolganRodionovSolver>(settings);
+    }
 
     throw std::runtime_error("Unknown solver type: " + settings.solver);
 }
